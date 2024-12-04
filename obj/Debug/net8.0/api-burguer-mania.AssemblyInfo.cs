9648093039e1bfc96f225c5305ca90eb42906d19
jvictordev1 <josevictorcruzrb@gmail.com>
2024-12-04 20:52:22 -0300

    chore: gitignore

--- obj/Debug/net8.0/api-burguer-mania.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-burguer-mania.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-burguer-mania")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37203e1055f66811295105d2d9fdda182debb662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f0b11d5077d6f65a5cb085e32a31286fbcb8ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-burguer-mania")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-burguer-mania")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
